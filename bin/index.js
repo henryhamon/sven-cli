7b56c9d13bac92fbbdd0a51e50a1c870a83705fb
Henry Hamon Rodrigues Pereira <henry.pereira@bplus.com.br.local>
2020-12-04 22:19:38 -0300

    Add first commands

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log( "Hello!" );
+const yargs = require("yargs");
+const axios = require("axios");
+
+const options = yargs
+  .usage("Usage: $0 <command> [options]")
+  .help('help')
+  .command("new [projectName]", "New project", (yargs) => {
+    yargs.positional('projectName', {describe: 'project name'})
+    .option("z", {alias: "zenclass", describe: "ZenClassname", type: 'string', demandOption: true})
+    .option("n", {alias: "namespace", describe: "namespace", type: 'string'})
+  })
+  .option("b", { alias: "build", describe: "Import build file to Zen class", type: "string" })
+  .option("s", { alias: "search", describe: "Search term", type: "string" })
+  .demandCommand(1, 'You need at least one command before moving on')
+  .alias('h', 'help')
+  .argv;
+
+
+console.log(options);
